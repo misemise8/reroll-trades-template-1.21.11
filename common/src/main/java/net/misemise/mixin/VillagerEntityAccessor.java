603db69476ba4ae24836e87b53c107169b896d84
misemise8 <semi.twitter42@gmail.com>
2026-06-14 12:50:38 +0900

    Fix 1.21.10 NeoForge trade update invoker

--- common/src/main/java/net/misemise/mixin/VillagerEntityAccessor.java
+++ common/src/main/java/net/misemise/mixin/VillagerEntityAccessor.java
@@ -1,6 +1,5 @@
 package net.misemise.mixin;
 
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.npc.AbstractVillager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
@@ -9,5 +8,5 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 public interface VillagerEntityAccessor {
 
     @Invoker("updateTrades")
-    void rerollTrades$updateTrades(ServerLevel level);
+    void rerollTrades$updateTrades();
 }
